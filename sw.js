ab7a9bf715a12e0155600dc604ebe235f5f915d4
jjuniordev18 <j.juniordev18@gmail.com>
2026-08-07 23:29:42 -0300

    fix: forçar ativação imediata do novo service worker + reload automático

--- sw.js
+++ sw.js
@@ -18,6 +18,10 @@ self.addEventListener('install', e => {
   );
 });
 
+self.addEventListener('message', e => {
+  if (e.data && e.data.type === 'SKIP_WAITING') self.skipWaiting();
+});
+
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys()
